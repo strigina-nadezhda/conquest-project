50c2eb92014229195f361775c4bba3d1e211a903
strigina-nadezhda <126505183+strigina-nadezhda@users.noreply.github.com>
2023-02-28 11:44:17 +0300

    Add files via upload

---
+++ slider/slider.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function changeSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  document.getElementById("id01").innerHTML = "0" + slideIndex;
+}
